18e33e6cb72296d0885b21f73b051b4fe99b057a
Kylan Lewis <45674401+kylanalewis@users.noreply.github.com>
2022-04-15 12:59:07 -0500

    test file

---
+++ execute-command.groovy
@@ -0,0 +1,9 @@
+ #!groovy
+ 
+def command = "date"
+def proc = command.execute()
+proc.waitFor()     
+
+println "Process exit code: ${proc.exitValue()}"
+println "Std Err: ${proc.err.text}"
+println "Std Out: ${proc.in.text}" 
